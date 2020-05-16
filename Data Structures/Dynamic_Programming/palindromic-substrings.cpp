22a950db1de1d6a1a0cc878386d5da2e40f11f1b
shubham4756 <60029456+shubham4756@users.noreply.github.com>
2020-05-16 23:10:45 +0530

    Update palindromic-substrings.cpp

--- Data Structures/Dynamic_Programming/palindromic-substrings.cpp
+++ Data Structures/Dynamic_Programming/palindromic-substrings.cpp
@@ -21,3 +21,32 @@ public:
         return ans;
     }
 };
+
+/*
+For find longest palindrom substring
+Link:https://leetcode.com/problems/longest-palindromic-substring/submissions/
+*/
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.size(),sz=0;
+        int idx=0;
+        vector<vector<bool> > palin(n,vector<bool>(n,true));
+        for(int j=1;j<n;j++){
+            for(int i=0;i<n-j;i++){
+                if(s[i]==s[i+j] && palin[i+1][i+j-1]){
+                   // cout<<i<<" "<<j<<" "<<i+j<<" "<<idx<<" "<<sz<<endl;
+                    palin[i][i+j]=true;
+                    if(sz<=j){
+                      idx=i;
+                      sz=j;
+                    }
+                }
+                else{
+                    palin[i][i+j]=false;
+                }
+            }
+        }
+        return s.substr(idx,sz+1);
+    }
+};
